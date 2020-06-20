51807da78bcb79ac475f8476d91e0c8c7c6ea235
yinghui.li <391509180@qq.com>
2020-06-20 22:37:17 +0800

    JApiDocs Try

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.github.yedaxia.apidocs.Docs;
+import io.github.yedaxia.apidocs.DocsConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class DemoApplication extends  SpringBootServletInitializer{
 
 	public static void main(String[] args) {
+
+		DocsConfig config = new DocsConfig();
+		config.setProjectPath("/SpringDemo"); // 项目根目录
+		config.setProjectName("SpringDemo"); // 项目名称
+		config.setApiVersion("V1.0");       // 声明该API的版本
+		config.setDocsPath("/API_Document"); // 生成API 文档所在目录
+		config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
+		Docs.buildHtmlDocs(config); // 执行生成文档
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/webController/HelloControlller.java
+++ src/main/java/com/example/demo/webController/HelloControlller.java
@@ -2,15 +2,22 @@ package com.example.demo.webController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/Hello")
 public class HelloControlller {
 
+
+    /**
+     * 测试
+     * @param name 姓名
+     * @return
+     */
     @ResponseBody
     @RequestMapping("/say")
-    public String sayHello(){
-        return "Hello, Daniel!";
+    public String sayHello(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
